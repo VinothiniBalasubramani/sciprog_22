d3ab59337af4793999c7ff2fc45d7bb22efeb26c
U-LAPTOP-R1GQQ67N\vino1 <vinothini.balasubramani@ucdconnect.ie>
2022-11-22 20:29:41 +0000

    Uploading practical09 files

---
+++ practical09/magic_square.h
@@ -0,0 +1,30 @@
+int isMagicSquare(int **square, const int n){
+
+	if(n<0){
+		return 0;
+	}
+	int magic_num=0;
+	int off_diag_sum = 0;
+	int i, j;
+	for(i=0; i<n; i++){
+		magic_num += square[i][i];
+		off_diag_sum += square[i][n-i-1];
+	}
+	if(off_diag_sum != magic_num)
+	{
+		return 0;
+	}
+	for(i=0; i<n; i++){
+		int rowsum = 0;
+		int columnsum = 0;
+		for(j=0; j<n; j++){
+			rowsum += square[i][j];
+			columnsum += square[j][i];
+		}
+		if(rowsum!=magic_num ||columnsum!=magic_num){
+			return 0;
+		}
+	}
+	return 1;
+
+}
